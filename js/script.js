06d9663b6b856f7876a9970be4141500e0e0220c
chiaraxs <chiaracardinale92@gmail.com>
2022-01-12 14:54:36 +0100

    Added: select option with icons type

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
-
-// Milestone 2
-// Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). 
-//Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
-// BONUS
-// 1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, 
-// sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-// 2- popolare le options della select della milestone 3 dinamicamente.
-
-
+// 1. creo array di oggetti
 const icons = [
 	
     {
@@ -130,14 +115,45 @@ const icons = [
 	}
 ];
 
-// 1. aggancio html
-const containerIcons = document.getElementById('container-icon')
 
-// 2. stampo tutte le cards icons
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+// Milestone 2
+// Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+// 2. aggancio html
+const iconsContainer = document.getElementById('container-icon')
+const optionContainer = document.querySelector('.option-container')
+
+// 3. stampo tutte le cards icons con forEach
 icons.forEach((element) => {
-    containerIcons.innerHTML += ` 
+   iconsContainer.innerHTML += ` 
     <div class="icons">
         <i class= "${element.family} ${element.prefix}${element.name} ${element.color}"> </i>
         <div>${element.name.toUpperCase()}</div>
     </div>`;
 })
+
+
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). 
+//Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+// 4. stampo in pagina select con options
+optionContainer.innerHTML += ` 
+<p>FILTRA PER TIPO
+<select id="options" name="options">
+    <option value="all">All</option>
+    <option value="animals">Animals</option>
+    <option value="vegetables">Vegetables</option>
+    <option value="user">User</option>
+</select>
+</p>`;
+
+
+
+
+// BONUS
+// 1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, 
+// sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+// 2- popolare le options della select della milestone 3 dinamicamente.
